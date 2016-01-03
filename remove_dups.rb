d69882546feddb34f81839fd428cfe347ae63686
Phil Bogle <phil@code.org>
2016-01-02 23:20:31 -0800

    Add remove_dups

---
+++ remove_dups.rb
@@ -0,0 +1,38 @@
+# Reads the resized duplicates in "resizes.json" and trashes all except the preferred duplicated.
+
+require_relative './drive'
+
+def main
+  client, drive = Drive.setup('remove_dups', '1.0.0')
+
+  file = File.open('resizes.json')
+  map = JSON.parse(file.read)
+
+  count = 0
+  for key, photos in map
+    preferred = photos.detect {|p| p['preferred'] == true}
+    raise "No preferred photo found" unless preferred
+    for photo in photos
+      if photo != preferred
+        count += 1
+        id = photo['id']
+        puts "#{count}. Trashing #{id}, keeping #{preferred['id']}"
+        trash_file(client, id)
+      end
+    end
+  end
+end
+
+def trash_file(client, file_id)
+  drive = client.discovered_api('drive', 'v2')
+  result = client.execute(
+    :api_method => drive.files.trash,
+    :parameters => { 'fileId' => file_id })
+  if result.status == 200
+    return result.data
+  else
+    puts "An error occurred: #{result.data['error']['message']}"
+  end
+end
+
+main
